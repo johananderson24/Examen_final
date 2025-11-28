0d01fba83587655b668e490fffc741fa936cccea
Denis <74616294@idexperujapon.edu.pe>
2025-11-28 18:34:22 -0500

    listo

--- Examen_final/Modelos/obsoletos.cs
+++ Examen_final/Modelos/obsoletos.cs
@@ -1,12 +1,111 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Examen_final.Modelos
 {
     internal class obsoletos
     {
+        public static DataTable obtener()
+        {
+            conexion cnn = new conexion();
+            try
+            {
+                cnn.conectar();
+                string consulta = "SELECT * FROM obsoletos";
+                SqlCommand cmd = new SqlCommand(consulta, cnn.conectar());
+                SqlDataAdapter adaptador = new SqlDataAdapter(cmd);
+                DataTable tabla = new DataTable();
+                adaptador.Fill(tabla);
+                return tabla;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al obtener obsoletos: " + ex.Message);
+                return null;
+            }
+            finally
+            {
+                cnn.desconectar();
+            }
+
+        }
+        public static bool Crear(string informe, string motivo, string disposicion)
+        {
+            conexion cnn = new conexion();
+            try
+            {
+                cnn.conectar();
+                string consulta = "INSERT INTO obsoletos (informe, motivo, disposicion) VALUES (@informe, @motivo, @disposicion)";
+                                 
+                SqlCommand cmd = new SqlCommand(consulta, cnn.conectar());
+                cmd.Parameters.AddWithValue("@informe",informe);
+                cmd.Parameters.AddWithValue("@motivo", motivo);
+                cmd.Parameters.AddWithValue("@costo", disposicion);
+                int filasAfectadas = cmd.ExecuteNonQuery();
+                return filasAfectadas > 0;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al crear el obsoleots: " + ex.Message);
+                return false;
+            }
+            finally
+            {
+                cnn.desconectar();
+            }
+        }
+        public static bool Editar(int obsoletos_id, string informe, string motivo, string disposicion)
+        {
+            conexion cnn = new conexion();
+            try
+            {
+                cnn.conectar();
+                string consulta = "UPDATE obsoletos SET informe=@informe, motivo=@motivo, disposicion=@disposicion WHERE obsoletos_id=@obsoletos_id";
+                SqlCommand cmd = new SqlCommand(consulta, cnn.conectar());
+                cmd.Parameters.AddWithValue("@informe", informe);
+                cmd.Parameters.AddWithValue("@motivo", motivo);
+                cmd.Parameters.AddWithValue("@disposicion", disposicion);
+                cmd.Parameters.AddWithValue("@obsoletos_id", obsoletos_id);
+                int filasAfectadas = cmd.ExecuteNonQuery();
+                return filasAfectadas > 0;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al editar obsoletos: " + ex.Message);
+                return false;
+            }
+            finally
+            {
+                cnn.desconectar();
+            }
+        }
+        public static bool Eliminar(int obsoletos_id)
+        {
+            conexion cnn = new conexion();
+            try
+            {
+                cnn.conectar();
+                string consulta = "DELETE FROM obsoletos WHERE obsoletos_id=@obsoletos_id";
+                SqlCommand cmd = new SqlCommand(consulta, cnn.conectar());
+                cmd.Parameters.AddWithValue("@obsoletos_id", obsoletos_id);
+                int filasAfectadas = cmd.ExecuteNonQuery();
+                return filasAfectadas > 0;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al eliminar obsoletos: " + ex.Message);
+                return false;
+            }
+            finally
+            {
+                cnn.desconectar();
+            }
+        }
     }
 }
